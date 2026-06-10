14ea6a9a69db676641daa84dcadc45be09fa7226
TetoTZ <sdeltadg3@gmail.com>
2026-06-10 15:28:32 -0600

    variables

---
+++ Get Started/variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int num = 15;
+    float pi = 3.14159;
+    char letra = 'a';
+    char* name = "waos";
+    char xd[] = "xd";
+    bool True = true;
+
+    printf("%d\n", num);
+    printf("%f\n", pi);
+    printf("%c\n", letra);
+    printf("%s\n", name);
+    printf("%s\n", xd);
+    printf("%d\n", True);
+    printf("my name is %s\n", name);
+    printf("my favorite number is %d\n", 35);
+    printf("the value of waos is %f\n", 2.00104);
+    printf("Mi letra favorita es %c\n", 'D');
+
+    return 0;
+}
